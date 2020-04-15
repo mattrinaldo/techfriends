00967defe99a968933f4cea4d0c62cf27477f117
Matthew Rinaldo <62995201+mattrinaldo@users.noreply.github.com>
2020-04-15 14:14:21 +1000

    Updates

---
+++ precache-manifest.d62b3948c22a82ab81fd33238e0c5b86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e15970461b949ea8c855bafe38326c",
+    "url": "/techfriends/index.html"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/techfriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "16eec61f2255eb661222",
+    "url": "/techfriends/static/css/main.b6b69c0c.chunk.css"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/techfriends/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/techfriends/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16eec61f2255eb661222",
+    "url": "/techfriends/static/js/main.a97139ed.chunk.js"
+  },
+  {
+    "revision": "cd2b220245c037ca86b2",
+    "url": "/techfriends/static/js/runtime-main.0a30dc7a.js"
+  },
+  {
+    "revision": "abc0738e1e9dc2baff102ee9c7d44986",
+    "url": "/techfriends/static/media/KMKDSPKW.abc0738e.woff"
+  }
+]);
